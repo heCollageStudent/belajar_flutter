4b58a0ba0c301012ebddc3892bb1289eb1a90671
dhirsyaram <dhirsyaramadhanpattah02@gmail.com>
2024-10-04 18:15:58 +0700

    first initiliaze repo flutter

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:wisatabandung/main_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
